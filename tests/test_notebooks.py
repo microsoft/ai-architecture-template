ef2b36442014d57a43ee22a04d14965f2673b55a
Dan <dciborow@microsoft.com>
2020-02-12 23:55:49 -0500

    Adding Pylint configuration

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -18,7 +18,7 @@ def test_00_aml_configuration():
 
     results = pm.execute_notebook(
         '../notebooks/00_AMLConfiguration.ipynb',
-        '../notebooks/00_AMLConfiguration.ipynb_output',
+        '../notebooks/00_AMLConfiguration.output_ipynb',
         parameters=dict(subscription_id=subscription_id, resource_group=resource_group, workspace_name=workspace_name,
                         workspace_region=workspace_region)
     )
