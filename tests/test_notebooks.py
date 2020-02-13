ce79aae06a75a326fa509ee662237e3298d97733
Dan <dciborow@microsoft.com>
2020-02-13 04:03:58 -0500

    Adding Pylint configuration

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -6,9 +6,10 @@ Licensed under the MIT License.
 """
 import papermill as pm
 from azure_utils.machine_learning.utils import load_configuration
+from junit_xml import TestSuite, TestCase
 
 
-def test_00_aml_configuration():
+def test_00_aml_configuration(capsys):
     cfg = load_configuration("../workspace_conf.yml")
 
     subscription_id = cfg['subscription_id']
@@ -27,3 +28,22 @@ def test_00_aml_configuration():
     for cell in results.cells:
         if cell.cell_type is "code":
             assert not cell.metadata.papermill.exception, "Error in Python Notebook"
+
+    regex = r'Deployed (.*) with name (.*) seconds.'
+
+    captured = capsys.readouterr()
+    output = captured.out
+
+    import re
+    searchObj = re.findall(regex, output)
+
+    test_cases = []
+
+    for group in searchObj:
+        test_cases += TestCase(name=group(1), classname='some.class.name', elapsed_sec=group(2), stdout=group(),
+                               status="Success")
+
+    ts = TestSuite("my test suite", test_cases)
+
+    with open('timing-test-output.xml', 'w') as f:
+        TestSuite.to_file(f, [ts], prettyprint=False)
